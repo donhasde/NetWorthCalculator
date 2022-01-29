bad343a9f8fa3ba8dc746f94c375a267981ec337
Don Ha <DonHaSDE@gmail.com>
2022-01-29 15:08:31 -0800

    Stubbed a basic front end

---
+++ networthcalculator-frontend/src/Content.js
@@ -0,0 +1,9 @@
+const Content = () => {
+    return (
+        <div className="Content">
+            <p>Stub</p>
+        </div>
+    );
+}
+ 
+export default Content;
---
+++ networthcalculator-frontend/src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Tracking your Networth</h1>
+        </nav>
+    );
+}
+ 
+export default Navbar;
