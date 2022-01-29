0c5bf11d4da068d1c933e05e9e2a2a17f9f0f6d6
Don Ha <DonHaSDE@gmail.com>
2022-01-28 17:45:10 -0800

    Switched to Spring boot for backend service development

---
+++ NetWorthCalculatorService/src/main/java/com/intuit/craftdemo/NetWorthCalculatorService/NetWorthCalculatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.craftdemo.NetWorthCalculatorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetWorthCalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetWorthCalculatorServiceApplication.class, args);
+	}
+
+}
---
+++ NetWorthCalculatorService/src/test/java/com/intuit/craftdemo/NetWorthCalculatorService/NetWorthCalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.craftdemo.NetWorthCalculatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetWorthCalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
